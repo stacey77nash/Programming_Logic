47051a276902d06c5d9bc3fa8f4240f3cfa665d7
Stacey Nash <stacey77nash@gmail.com>
2021-02-03 23:07:05 -0600

    First Python program
    Print out the lyrics to my least favorite song.

---
+++ LeastFavoriteSong.py
@@ -0,0 +1,60 @@
+#Program Name: Least Favorite Song
+#Course: ITSE-1302-20Z1
+#Author: Stacey Nash
+#Description: This program prints out the lyrics to one of my least favorite songs.
+
+print("Song: I Got A Feeling")
+print("Artist: The Black-Eyed Peas")
+print()
+print("I gotta feeling")
+print("That tonight's gonna be a good night")
+print("That tonight's gonna be a good night")
+print("That tonight's gonna be a good night")
+print("A feeling")
+print("That tonight's gonna be a good night")
+print("That tonight's gonna be a good night")
+print("That tonight's gonna be a good, good night")
+print("A feeling (Ooo-hoo)")
+print("That tonight's gonna be a good night")
+print("That tonight's gonna be a good night")
+print("That tonight's gonna be a good, good night")
+print("A feeling (Ooo-hoo)")
+print("That tonight's gonna be a good night")
+print("That tonight's gonna be a good night")
+print("That tonight's gonna be a good, good night")
+print("A feel...")
+print("Tonight's the night")
+print("Let's live it up")
+
+#Remainder of the song lyrics in a multi-line comment to abreviate the output
+
+"""
+Tonight's the night
+Let's live it up
+I got my money
+Let's spend it up
+Go out and smash it
+Like, oh my God!
+Jump off that sofa
+Let's get - get off
+I know that we'll have a ball
+If we get down and go out
+And just lose it all
+I feel stressed and
+I wanna let it go
+Let's go way out spaced out
+And losing all control
+Fill up my cup
+Mazel tov
+Look at her dancin'
+Just take it... off
+Let's paint the town
+We'll shut it down
+Let's burn the roof
+And then we'll do it again
+Let's do it, let's do it, let's do it
+Let's do it, and do it, and do it
+Let's live it up
+And do it, and do it, and do it, do it, do it
+Let's do it, let's do it, let's do it") 
+"""
